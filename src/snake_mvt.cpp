33e28fcce415fbad334b02f7f78a3b2c1ea610ad
Paul P <paul.phamdang@gmail.com>
2025-01-05 22:50:35 +0000

    Node finished

--- src/snake_mvt.cpp
+++ src/snake_mvt.cpp
@@ -1,10 +1,60 @@
-#include <cstdio>
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 
-int main(int argc, char ** argv)
+class SnakeMvtNode : public rclcpp::Node
 {
-  (void) argc;
-  (void) argv;
+public:
+  SnakeMvtNode() : Node("snake_mvt_node")
+  {
+    // Publisher to publish Twist messages to /cmd_vel topic
+    publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+    // Subscription to /odom topic to get the position of the robot
+    subscription_ = this->create_subscription<nav_msgs::msg::Odometry>(
+      "/odom", 10, std::bind(&SnakeMvtNode::odom_callback, this, std::placeholders::_1));
+  }
 
-  printf("hello world rt1_assignment2_2 package\n");
+private:
+  void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
+  {
+    auto twist = geometry_msgs::msg::Twist();
+    double x = msg->pose.pose.position.x;
+    double y = msg->pose.pose.position.y;
+
+    // If the robot is out of the map, stop the robot 
+    // else move the robot depending on its position
+    if (y <1.0 || y>9.0)
+    {
+      twist.linear.x = 0.0;
+      twist.angular.z = 0.0;
+    }
+    else if (x > 8.5)
+    {
+      twist.linear.x = 1.0;
+      twist.angular.z = 2.0;
+    }
+    else if (x < 2.0)
+    {
+      twist.linear.x = 1.0;
+      twist.angular.z = -2.0;
+    }
+    else
+    {
+      twist.linear.x = 1.0;
+      twist.angular.z = 0.0;
+    }
+
+    publisher_->publish(twist);
+  }
+
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscription_;
+};
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<SnakeMvtNode>());
+  rclcpp::shutdown();
   return 0;
 }
